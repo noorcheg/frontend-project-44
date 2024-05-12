728288b5c227d6f5af0306053ea4721805505696
Ainur Galiakberov <noorcheg@gmail.com>
2024-05-12 22:02:14 +0500

    fix: add function calculate

--- src/games/game-calc.js
+++ src/games/game-calc.js
@@ -1,5 +1,18 @@
 import getRandomNumber from '../utils.js';
 
+const calculate = (firstNumber, secondNumber, operation) => {
+  switch (operation) {
+    case '+':
+      return firstNumber + secondNumber;
+    case '-':
+      return firstNumber - secondNumber;
+    case '*':
+      return firstNumber * secondNumber;
+    default:
+      return 'error';
+  }
+};
+
 const createGameRules = () => 'What is the result of the expression?.';
 
 const createGameData = () => {
@@ -12,18 +25,11 @@ const createGameData = () => {
     const firstNumber = getRandomNumber(minNumber, maxNumber);
     const secondNumber = getRandomNumber(minNumber, maxNumber);
     const operations = ['+', '-', '*'];
-    const operationNumber = Math.floor(Math.random() * operations.length);
-    const question = `${firstNumber} ${operations[operationNumber]} ${secondNumber}`;
-    let correctAnswer;
-    if (operationNumber === 0) {
-      correctAnswer = firstNumber + secondNumber;
-    } else if (operationNumber === 1) {
-      correctAnswer = firstNumber - secondNumber;
-    } else if (operationNumber === 2) {
-      correctAnswer = firstNumber * secondNumber;
-    }
+    const operation = operations[getRandomNumber(0, operations.length - 1)];
+    const question = `${firstNumber} ${operation} ${secondNumber}`;
+    const correctAnswer = calculate(firstNumber, secondNumber, operation).toString();
     questions.push(question);
-    correctAnswers.push(String(correctAnswer));
+    correctAnswers.push(correctAnswer);
   }
   return [questions, correctAnswers];
 };
