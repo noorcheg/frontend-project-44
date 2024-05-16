29ab5fbc4785033dcc53cc5b2a01c8ad1137b4d7
Ainur Galiakberov <noorcheg@gmail.com>
2024-05-16 16:27:43 +0500

    fix: function isPrime

--- src/games/game-prime.js
+++ src/games/game-prime.js
@@ -1,23 +1,24 @@
 import getRandomNumber from '../utils.js';
 
+const isPrime = (num) => {
+  if (num <= 1) return false;
+  for (let i = 2; i <= Math.sqrt(num); i += 1) {
+    if (num % i === 0) return false;
+  }
+  return true;
+};
+
 const createGameRules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const createGameData = () => {
   const minNumber = 1;
   const maxNumber = 99;
   const questionNumber = getRandomNumber(minNumber, maxNumber);
-  const numberOfIterations = Math.floor(questionNumber / 2);
-  let maxDivider;
-  for (let j = 1; j <= numberOfIterations; j += 1) {
-    if (questionNumber % j === 0) {
-      maxDivider = j;
-    }
-  }
   let correctAnswer;
-  if (maxDivider > 1) {
-    correctAnswer = 'no';
-  } else {
+  if (isPrime(questionNumber)) {
     correctAnswer = 'yes';
+  } else {
+    correctAnswer = 'no';
   }
 
   return [questionNumber, correctAnswer];
